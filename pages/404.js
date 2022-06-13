5cf9330122df29ce4536d4577b1a9cd8adf430a4
Gavryk <gvozdyk23@gmail.com>
2022-06-14 01:00:30 +0300

    just for test next.js

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,17 @@
 import Link from 'next/link';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 import style from '../styles/NotFound.module.scss';
 
 const NotFoundPage = () => {
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000);
+    }, []);
+
+
   return (
     <div className={style.notFound}>
       <div className={style.text}>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import style from '../styles/Home.module.scss'
 
 const Home = () => {
 
   return (
-    <div className={style.homePage}>
-      <div className="container">
-        <h1>Home Page</h1>
+    <>
+      <Head>
+        <title>Fat Burgers | Home</title>
+        <meta name="title" content="Fat Burgers"></meta>
+      </Head>
+      <div className={style.homePage}>
+        <div className="container">
+          <div className={style.homeTitle}>
+            <h1>Main Page</h1>
+          </div>
+          <div className={style.homeImage}>
+            <Image
+              src="/burger-10909.png"
+              alt="main burger"
+              width={400}
+              height={300}
+            />
+          </div>
+          <div className={style.homeText}>
+            <p>Choose your Burger</p>
+            <Link href="/burgers">
+              <a className={style.homeButton}>Burgers</a>
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
